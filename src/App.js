3031b783592b904e59d2611a901fd98659a000d3
ThePhiri <chipoephiri@gmail.com>
2022-07-14 03:20:36 +0200

    added veggie recipes plus popular recipes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Pages from "./pages/Pages";
+import './index.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hau Gogo, You are here</h1>
+      <Pages/>
+    </div>
+  );
+}
+
+export default App;
