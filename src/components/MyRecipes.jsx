475965213a82f81cde064ab8c4ed8399d7ea795e
ThePhiri <chipoephiri@gmail.com>
2022-09-28 22:06:53 +0200

    wip: add my recipes end point and check for empty list

--- src/components/MyRecipes.jsx
+++ src/components/MyRecipes.jsx
@@ -3,21 +3,20 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 const MyRecipes = () => {
-    const [foodList, setMyRecipes] = useState([])
+    const [recipesList, setMyRecipes] = useState([])
     const params = useParams();
 
     useEffect(() => {
         getMyRecipes();
-        console.log("foods", foodList);
     }, []);
 
     const getMyRecipes = async () => {
         const token = localStorage.getItem("token")
-        console.log("toekn", "https://gogos-recipes-backend.herokuapp.com/api/recipes/user/" + params.id + "?" + token);
+
 
         var myHeaders = new Headers();
         myHeaders.append("token", token);
-        var raw = "";
+
 
         var requestOptions = {
             method: 'GET',
@@ -29,15 +28,15 @@ const MyRecipes = () => {
         const data = await api.json();
         setMyRecipes(data.data);
 
-
     }
     return (
         <div className='max-w-[1640px] mx-auto p-4 py-12'>
             <h1 className='text-orange-600 font-bold text-4xl text-center'>My Recipes</h1>
 
-            <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
 
-                {foodList.map((item, index) => (
+
+            {recipesList ? recipesList.map((item, index) => (
+                <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
                     <div key={index} className='border shadow-lg hover:scale-105 duration-300 rounded-lg'>
                         <img src={item.image} alt={item.name} className='w-full h-[200px] object-cover rounded-t-lg' />
                         <div className='flex justify-between px-2 py-4'>
@@ -45,12 +44,11 @@ const MyRecipes = () => {
                             <Link to={"/recipe/" + item._id}><button>View</button></Link>
                         </div>
                     </div>
-                ))}
+                </div>
+            )) : <div className='flex justify-center '><p>No recipes. Add some now </p><img src="https://miro.medium.com/max/1600/1*XWuUx3XuZHJDbyzE8Muysw.gif" alt="no image" /></div>}
 
-            </div>
 
-
-        </div>
+        </div >
     )
 }
 
