3b4bb5a541c36a44ac774946d46b56251275b6b6
ThePhiri <chipoephiri@gmail.com>
2023-01-31 19:45:34 +0200

    linting error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import HeroSection from "../components/HeroSection";
 import ImproveSkillsSection from "../components/ImproveSkillsSection";
 import QuoteSection from "../components/QuoteSection";
-import TopSection from "../components/TopSection";
+// import TopSection from "../components/TopSection";
 
 
 function Home() {
